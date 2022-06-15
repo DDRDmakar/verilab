6614c31d52c40d65bfe399550077ed27654407ac
DDRDmakar <makarevich.98@mail.ru>
2022-06-15 16:29:49 +0300

    Update verilab.py

--- verilab.py
+++ verilab.py
@@ -37,6 +37,11 @@ def to_bin_str(val, width=None):
 	if (width == None): return '{0:b}'.format(val)
 	else:               return '{0:0{width}b}'.format(val, width=width)
 
+# Int to hex string
+def to_hex_str(val, width=None):
+	if (width == None): return '{0:x}'.format(val)
+	else:               return '{0:0{width}x}'.format(val, width=width)
+
 def rand_sint_nbits(nbits, size=None):
 	np.random.randint(low=-(2**(nbits-1)), high=+(2**(nbits-1)), size=size)
 
@@ -60,15 +65,15 @@ def rand_uint_nbits(nbits, size=None):
 #	
 #	for i in range(256):
 #		N = 16
-#		D = rand_sint_nbits(N, size=4)
+#		D = rand_uint_nbits(N, size=4)
 #		RES = np.sort(D)
 #		
-#		print(to_ubin_str(D[0], N), file=files[0])
-#		print(to_ubin_str(D[1], N), file=files[1])
-#		print(to_ubin_str(D[2], N), file=files[2])
-#		print(to_ubin_str(D[3], N), file=files[3])
+#		print(to_bin_str(D[0], N), file=files[0])
+#		print(to_bin_str(D[1], N), file=files[1])
+#		print(to_bin_str(D[2], N), file=files[2])
+#		print(to_bin_str(D[3], N), file=files[3])
 #		
-#		print(to_ubin_str(RES[0], N), file=files[4])
-#		print(to_ubin_str(RES[1], N), file=files[5])
-#		print(to_ubin_str(RES[2], N), file=files[6])
-#		print(to_ubin_str(RES[3], N), file=files[7])
+#		print(to_bin_str(RES[0], N), file=files[4])
+#		print(to_bin_str(RES[1], N), file=files[5])
+#		print(to_bin_str(RES[2], N), file=files[6])
+#		print(to_bin_str(RES[3], N), file=files[7])
