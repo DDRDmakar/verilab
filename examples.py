e28e397cbd55984954508c6da08d5db737c5ea0a
Nikita Makarevich <nikita.makarevich@spbpu.com>
2023-10-30 13:02:57 +0300

    more functions

--- examples.py
+++ examples.py
@@ -1,5 +1,11 @@
 # Usage example
 
+from contextlib import ExitStack
+
+from verilab import *
+
+#========[ Generate dataset for testing HDL module sorting 4 numbers ]========#
+
 fnames = [
 	'D0.dat',
 	'D1.dat',
@@ -29,19 +35,43 @@ with ExitStack() as stack:
 		print(to_bin_str(RES[2], N), file=files[6])
 		print(to_bin_str(RES[3], N), file=files[7])
 
-#================================================================================#
 
-data = pd.read_csv("data.csv")
-cs = data['FW']
-clk = data['sym_pol']
-mosi = data['Dch_ena']
-miso = data['IFW']
+#========[ Example reading CSV file with pandas ]========#
+# 
+# data = pd.read_csv("data.csv")
+# a = data['FW']
+# b = data['sym_pol']
 
-datas = [cs, clk, mosi, miso]
 
-with ExitStack() as stack:
-	files = [stack.enter_context(open(fname, 'w')) for fname in fnames_event[:-1]]
-	for f, d in zip(files, datas):
-		print(f)
-		for sample in d[start_index:end_index]:
-			print(to_bin_str(sample, 1), file=f)
+#========[ Other ]========#
+
+print('to_uint')
+for e in [to_uint(i, 8) for i in [-4, 4]]:
+	print(to_hex_str(e, 8), to_bin_str(e, 8))
+
+print('to_sint')
+for e in [to_sint(i, 8) for i in [252, 253, 254, 255, 0, 1, 2, 3]]:
+	print(to_hex_str(e, 8), to_bin_str(e, 8))
+
+print('rand_sint_nbits')
+print(rand_sint_nbits(4, 10))
+
+print('rand_uint_nbits')
+print(rand_uint_nbits(4, 10))
+
+print('manchester_encode')
+b = np.random.randint(0, 2, 20, dtype=np.int8)
+m = manchester_encode(b)
+print(b)
+print(m)
+
+print('manchester_decode')
+b2 = manchester_decode(m)
+print(b2)
+
+print('to_gray')
+for e in [to_gray(i) for i in range(10)]:
+	print(to_hex_str(e, 8), to_bin_str(e, 4))
+print('from_gray')
+for e in [from_gray(i) for i in range(10)]:
+	print(to_hex_str(e, 8), to_bin_str(e, 4))
--- verilab.py
+++ verilab.py
@@ -1,6 +1,12 @@
 
+#=======================================#
+# Hardware evaluation helping programs
+# Nikita Makarevich (a.k.a DDRDmakar)
+# 2023
+# dedrtos@gmail.com
+#=======================================#
+
 import numpy as np
-from contextlib import ExitStack
 
 # 7-segment code
 # 7 bits:
@@ -42,8 +48,67 @@ def to_hex_str(val, width=None):
 	if (width == None): return '{0:x}'.format(val)
 	else:               return '{0:0{width}x}'.format(val, width=int(np.ceil(width/4)))
 
+# Generate random signed int with given bit width
+# If size != None, generate array of random values
 def rand_sint_nbits(nbits, size=None):
-	np.random.randint(low=-(2**(nbits-1)), high=+(2**(nbits-1)), size=size)
+	return np.random.randint(low=-(2**(nbits-1)), high=+(2**(nbits-1)), size=size)
 
+# Generate random unsigned ints
+# ----//----
 def rand_uint_nbits(nbits, size=None):
-	np.random.randint(low=0, high=+(2**nbits), size=size)
+	return np.random.randint(low=0, high=+(2**nbits), size=size)
+
+# Encode data with manchester code
+# input and output: numpy arrays of 0/1 values
+# len(output) = 2 * len(input)
+def manchester_encode(d):
+	m = np.ndarray(len(d)*2, dtype=type(d[0]))
+	pos = 0
+	for b in d:
+		if b:
+			m[pos] = 1
+			m[pos+1] = 0
+		else:
+			m[pos] = 0
+			m[pos+1] = 1
+		pos += 2
+	return m
+
+# Decode manchester code
+# input and output: numpy arrays of 0/1 values
+# len(input) = 2 * len(output)
+def manchester_decode(m):
+	d = np.ndarray(len(m)//2, dtype=type(m[0]))
+	b2 = 0b00
+	for i, b in enumerate(m):
+		b2 = (b2 << 1) & 0b11 | b
+		if i % 2:
+			if b2 == 0b01: d[i // 2] = 0
+			elif b2 == 0b10: d[i // 2] = 1
+			else: break
+	return d
+
+# Shift elements in numpy arary
+# returns shifted array
+def shift_np_array(xs, n, cval=np.nan):
+	e = np.empty_like(xs)
+	if n >= 0:
+		e[:n] = cval
+		e[n:] = xs[:-n]
+	else:
+		e[n:] = cval
+		e[:n] = xs[-n:]
+	return e
+
+# Encode int with corresponding gray code
+def to_gray(x):
+	return x ^ (x >> 1)
+
+# Decode int from gray code
+def from_gray(x):
+	a = x
+	mask = x
+	while mask != 0:
+		mask >>= 1
+		a ^= mask
+	return a
